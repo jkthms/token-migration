47a3a472bc47e0b6468d65f95dbce2fbb9259500
jkthms <jakeleediffusion@gmail.com>
2025-01-12 12:44:40 +0000

    chore: Push unstaged changes

--- src/ValiDAO.sol
+++ src/ValiDAO.sol
@@ -7,9 +7,11 @@ pragma solidity ^0.8.20;
 /// @dev Mints initial supply to deployer and to the migration contract
 /// @custom:security-contact [TODO]
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract ValiDAO is ERC20 {
+contract ValiDAO is Initializable, ERC20Upgradeable, OwnableUpgradeable {
     constructor(string memory name, string memory symbol, address treasury, uint256 totalSupply) ERC20(name, symbol) {
         // Mint the total supply to the treasury
         _mint(treasury, totalSupply);
